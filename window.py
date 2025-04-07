adccd236aaa506e7313d1e0269b8936ea72f3d0f
Jlaporte25 <jacoblaporte25@gmail.com>
2025-04-07 13:54:25 -0500

    finished window class and main opens window and waits for close

--- window.py
+++ window.py
@@ -6,3 +6,19 @@ class Window:
         self.width = width
         self.height = height
         self.__root = Tk()
+        self.title = self.__root.wm_title()
+        self.canvas = Canvas().pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running is True:
+            self.redraw()
+
+    def close(self):
+        self.running = False
