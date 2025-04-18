4ec29ad6edc00bc80365118ecee18a29f0cf253e
Jlaporte25 <jacoblaporte25@gmail.com>
2025-04-18 12:31:59 -0500

    had to use a seer stone...

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from graphics import Window, Cell
+from graphics import Window
+from maze import Maze
 
 
 def main():
-    win = Window(800, 600)
-    cell = Cell(200, 250, 400, 300, win)
-    cell2 = Cell(500, 300, 600, 400, win)
-    cell.draw()
-    cell2.draw()
-    cell.draw_move(cell2)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
     win.wait_for_close()
 
 
