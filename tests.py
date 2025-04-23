1faf336da7c0b8a2f4fbe003b4b9d620c6cb07af
Jlaporte25 <jacoblaporte25@gmail.com>
2025-04-23 17:50:35 -0500

    wrote more tests

--- tests.py
+++ tests.py
@@ -16,6 +16,20 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_create_cells_2(self):
+        num_cols = 9
+        num_rows = 5
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+
+    def test_maze_create_cells_3(self):
+        num_cols = 20
+        num_rows = 15
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(len(m1._cells), num_cols)
+        self.assertEqual(len(m1._cells[0]), num_rows)
+
 
 if __name__ == "__main__":
     unittest.main()
