07d44996038348a276da99faa4231b6b2f4ba7a0
Jlaporte25 <jacoblaporte25@gmail.com>
2025-04-24 19:17:51 -0500

    finished break entrance and exit function and added test

--- maze.py
+++ maze.py
@@ -24,6 +24,7 @@ class Maze:
         self._win = win
 
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -52,4 +53,9 @@ class Maze:
         time.sleep(0.05)
 
     def _break_entrance_and_exit(self):
-        pass
+        entrance_cell = self._cells[0][0]
+        exit_cell = self._cells[(self._num_cols - 1)][(self._num_rows - 1)]
+        entrance_cell.has_top_wall = False
+        exit_cell.has_bottom_wall = False
+        self._draw_cell(0, 0)
+        self._draw_cell((self._num_cols - 1), (self._num_rows - 1))
