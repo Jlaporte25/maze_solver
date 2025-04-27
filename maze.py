9bedd444a77a84bbc1ccb932a59256effa4042f4
Jlaporte25 <jacoblaporte25@gmail.com>
2025-04-26 19:02:30 -0500

    started break walls random function

--- maze.py
+++ maze.py
@@ -5,14 +5,7 @@ import time
 
 class Maze:
     def __init__(
-        self,
-        x1,
-        y1,
-        num_rows,
-        num_cols,
-        cell_size_x,
-        cell_size_y,
-        win=None,
+        self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None
     ):
         self._cells = []
         self._x1 = x1
@@ -22,9 +15,12 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        self.seed = seed
 
         self._create_cells()
         self._break_entrance_and_exit()
+        if self.seed is not None:
+            random.seed(seed)
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -59,3 +55,6 @@ class Maze:
         exit_cell.has_bottom_wall = False
         self._draw_cell(0, 0)
         self._draw_cell((self._num_cols - 1), (self._num_rows - 1))
+
+    def _break_walls_random(self, i, j):
+        pass
